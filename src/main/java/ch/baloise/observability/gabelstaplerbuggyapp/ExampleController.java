d90f300b6cc7a1a6ee39b32ff74429a297430089
David Weber <david.weber@w3tec.ch>
2023-10-17 15:01:33 +0200

    chore: add tracing endpoint

--- src/main/java/ch/baloise/observability/gabelstaplerbuggyapp/ExampleController.java
+++ src/main/java/ch/baloise/observability/gabelstaplerbuggyapp/ExampleController.java
@@ -16,69 +16,61 @@ public class ExampleController {
 
     @GetMapping("/example")
     String example() throws Exception {
-        return Pyroscope.LabelsWrapper.run(new LabelsSet("controller", "ExampleController.example"), () -> {
-            log.info("ExampleController.example endpoint called");
-            return "normal";
-        });
+        log.info("ExampleController.example endpoint called");
+        return "normal";
     }
 
     @GetMapping("/example/long")
     String exampleLong() throws Exception {
-        return Pyroscope.LabelsWrapper.run(new LabelsSet("controller", "ExampleController.exampleLong"), () -> {
-            log.info("ExampleController.exampleLong endpoint called");
-            Thread.sleep(15000);
-            return "bubu long";
-        });
+        log.info("ExampleController.exampleLong endpoint called");
+        Thread.sleep(15000);
+        return "bubu long";
     }
 
     @GetMapping("/example/cpu")
     String exampleCpu() throws Exception {
-        return Pyroscope.LabelsWrapper.run(new LabelsSet("controller", "ExampleController.exampleCpu"), () -> {
-            log.info("ExampleController.exampleCpu endpoint called");
-            var thread1 = new CPUSpikerThread();
-            thread1.start();
-            var thread2 = new CPUSpikerThread();
-            thread2.start();
-            var thread3 = new CPUSpikerThread();
-            thread3.start();
-            var thread4 = new CPUSpikerThread();
-            thread4.start();
-            var thread5 = new CPUSpikerThread();
-            thread5.start();
-            Thread.sleep(15000);
-            thread1.stop();
-            thread2.stop();
-            thread3.stop();
-            thread4.stop();
-            thread5.stop();
-            return "bubu cpu";
-        });
+        log.info("ExampleController.exampleCpu endpoint called");
+        var thread1 = new CPUSpikerThread();
+        thread1.start();
+        var thread2 = new CPUSpikerThread();
+        thread2.start();
+        var thread3 = new CPUSpikerThread();
+        thread3.start();
+        var thread4 = new CPUSpikerThread();
+        thread4.start();
+        var thread5 = new CPUSpikerThread();
+        thread5.start();
+        Thread.sleep(15000);
+        thread1.stop();
+        thread2.stop();
+        thread3.stop();
+        thread4.stop();
+        thread5.stop();
+        return "bubu cpu";
     }
 
     @GetMapping("/example/memory")
     String exampleMemory() throws Exception {
-        return Pyroscope.LabelsWrapper.run(new LabelsSet("controller", "ExampleController.exampleMemory"), () -> {
-            log.info("ExampleController.exampleMemory endpoint called");
-            var counter = 0;
-            while (counter < 1000000) {
-                myMap.put("key" + counter, "Large stringgggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
-                        + counter);
-                log.info("inserted key in map key" + counter);
-                ++counter;
-            }
-            return "bubu memory";
-        });
+        log.info("ExampleController.exampleMemory endpoint called");
+        var counter = 0;
+        while (counter < 1000000) {
+            myMap.put("key" + counter, "Large stringgggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + "ggggggggggggggggggggggggggggggggggggggggggggggggggggg"
+                    + counter);
+            log.info("inserted key in map key" + counter);
+            ++counter;
+        }
+        return "bubu memory";
     }
 
     public static class CPUSpikerThread extends Thread {
---
+++ src/main/java/ch/baloise/observability/gabelstaplerbuggyapp/ProfilingTracingFilter.java
@@ -0,0 +1,39 @@
+package ch.baloise.observability.gabelstaplerbuggyapp;
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.pyroscope.labels.LabelsSet;
+import io.pyroscope.labels.Pyroscope;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class ProfilingTracingFilter implements WebFilter {
+
+    @Autowired
+    Tracer tracer;
+
+    @SneakyThrows
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange,
+                             WebFilterChain webFilterChain) {
+        String traceId = "unknown";
+        String spanId = "unknown";
+        Span span = tracer.currentSpan();
+        if (span != null) {
+            traceId = span.context().traceId();
+            spanId = span.context().spanId();
+        }
+        log.info("Filter called with trace-id %s and span-id %s".formatted(traceId, spanId));
+
+        return Pyroscope.LabelsWrapper.run(new LabelsSet("trace-id", traceId, "span-id", spanId), () -> webFilterChain.filter(serverWebExchange));
+    }
+
+}
