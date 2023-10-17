d493411243accd2ba9f1ac082dd626a7f55a9542
David Weber <david.weber@w3tec.ch>
2023-10-17 13:48:54 +0200

    chore: add tracing endpoint

--- src/main/java/ch/baloise/observability/gabelstaplerbuggyapp/PyroscopeTracingAspect.java
+++ src/main/java/ch/baloise/observability/gabelstaplerbuggyapp/PyroscopeTracingAspect.java
@@ -9,10 +9,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@EnableAspectJAutoProxy
 @Slf4j
 public class PyroscopeTracingAspect {
 
